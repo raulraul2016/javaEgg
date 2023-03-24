371cf69f41f5b7be00b3ca90e917c6398396c09b
raulraul2016 <raul.sistemas2012@gmail.com>
2023-03-23 23:37:30 -0300

    Ejercicio 1 tipos de variables

---
+++ src/javaintroej1/JavaIntroEj1.java
@@ -0,0 +1,26 @@
+
+package javaintroej1;
+
+/**
+ *
+ * @author bangho
+ */
+public class JavaIntroEj1 {
+
+    
+    public static void main(String[] args) {
+        
+        char letra;
+        int num;
+        float prom;
+        String nombreApellido;
+        long ganancia;
+        boolean b;
+        
+        String nombre;
+        boolean bandera;
+        char vocal;
+        
+    }
+    
+}
